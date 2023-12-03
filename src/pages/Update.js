192e38e17fda3d4a52156404a5606562dd629386
jeanchaneto <92741347+jeanchaneto@users.noreply.github.com>
2023-12-03 12:32:40 +0100

    Update single record

--- src/pages/Update.js
+++ src/pages/Update.js
@@ -4,10 +4,11 @@ import supabase from "../config/supabaseClient";
 
 const Update = () => {
   const { id } = useParams();
-  const naviguate = useNavigate();
+  const navigate = useNavigate();
   const [title, setTitle] = useState("");
   const [method, setMethod] = useState("");
   const [rating, setRating] = useState("");
+  const [formError, setFormError] = useState(null);
 
   useEffect(() => {
     const fetchSmoothie = async () => {
@@ -18,39 +19,62 @@ const Update = () => {
         .eq("id", id)
         .single();
       if (error) {
-        naviguate("/", { replace: true });
+        navigate("/", { replace: true });
       }
 
       if (data) {
         setTitle(data.title);
         setMethod(data.method);
         setRating(data.rating);
-        console.log(data);
       }
     };
     fetchSmoothie();
-  }, [id, naviguate]);
+  }, [id, navigate]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!title || !method || !rating) {
+      setFormError("Please fill in all the fields");
+      return;
+    }
+    //update single record
+    const { data, error } = await supabase
+      .from("smoothies")
+      .update({ title, method, rating })
+      .eq("id", id)
+      .select();
+
+    if (error) {
+      console.log(error);
+      setFormError("Error updating");
+    }
+
+    if (data) {
+      setFormError(null);
+      navigate("/");
+    }
+  };
 
   return (
     <div className="page update">
-      <form>
+      <form onSubmit={handleSubmit}>
         <label htmlFor="title">Title:</label>
-        <input 
-          type="text" 
+        <input
+          type="text"
           id="title"
           value={title}
           onChange={(e) => setTitle(e.target.value)}
         />
 
         <label htmlFor="method">Method:</label>
-        <textarea 
+        <textarea
           id="method"
           value={method}
           onChange={(e) => setMethod(e.target.value)}
         />
 
         <label htmlFor="rating">Rating:</label>
-        <input 
+        <input
           type="number"
           id="rating"
           value={rating}
@@ -58,6 +82,7 @@ const Update = () => {
         />
 
         <button>Update Smoothie Recipe</button>
+        {formError && <p className="error">{formError}</p>}
       </form>
     </div>
   );
