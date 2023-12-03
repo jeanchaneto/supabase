918efa6793e9f0ec100d8d07289fb0fe1c196fde
jeanchaneto <92741347+jeanchaneto@users.noreply.github.com>
2023-12-03 11:31:26 +0100

    Writing data

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,41 @@
-import supabase from "../config/supabaseClient"
+import { useEffect, useState } from "react";
+import supabase from "../config/supabaseClient";
+import SmoothieCard from "../components/SmoothieCard";
 
 const Home = () => {
-console.log(supabase);
+  const [error, setError] = useState(null);
+  const [smoothies, setSmoothies] = useState([]);
+
+  useEffect(() => {
+    const fetchSmoothies = async () => {
+      const { data, error } = await supabase.from("smoothies").select();
+      if (error) {
+        setError("Could not fetch the smoothies");
+        setSmoothies(null);
+        console.log(error);
+      }
+      if (data) {
+        setSmoothies(data);
+        setError(null);
+      }
+    };
+    fetchSmoothies();
+  }, []);
 
   return (
     <div className="page home">
-      <h2>Home</h2>
+      {error && <p>{error}</p>}
+      {smoothies && (
+        <div className="smoothies">
+          <div className="smoothie-grid">
+            {smoothies.map((smoothie) => (
+              <SmoothieCard smoothie={smoothie} key={smoothie.id} />
+            ))}
+          </div>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
